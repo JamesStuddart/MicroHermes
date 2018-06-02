a1fe2be7ff0e812f404c84329c4d636ae9b7a62b
James Studdart <james@studdart.co.uk>
2018-06-02 23:57:42 +0100

    added in start to unit tests for commands

--- MicroHermes.VehiclesTests/Data/Commands/VehicleCommandsTests.cs
+++ MicroHermes.VehiclesTests/Data/Commands/VehicleCommandsTests.cs
@@ -1,7 +1,31 @@
-﻿namespace MicroHermes.VehiclesTests.Data.Commands
+﻿using MicroHermes.Vehicles.Core.Data.Commands;
+using MicroHermes.Vehicles.Core.Entities;
+using Moq;
+using Should;
+using Xunit;
+
+namespace MicroHermes.VehiclesTests.Data.Commands
 {
     public class VehicleCommandsTests
     {
-        
+        private readonly IVehicleCommands _vehicleCommands;
+
+        public VehicleCommandsTests()
+        {
+            _vehicleCommands = new VehicleCommands();
+        }
+
+        [Fact]
+        public void SDHP_UpdateVehicle()
+        {
+            //Arrange
+            var entity = new VehicleEntity();
+
+            //Act
+            var result = _vehicleCommands.UpdateVehicle(entity);
+
+            //Assert
+            result.ShouldBeTrue();
+        }
     }
 }
